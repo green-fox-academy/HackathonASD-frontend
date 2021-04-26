7d04cc4ec91e1f7381eb66ede8ea603b5e79d236
Mate Marko <markomate90@gmail.com>
2021-04-26 18:12:44 +0200

    added some style to landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*async function loadItems() {
+  const BACKEND_URL = await fetch("/api").then((res) => {
+    return res.text();
+  });
+
+  const response = await fetch(BACKEND_URL + "/page");
+
+  return response;
+}
+
+const items = loadItems();*/
+
+router.get("/", function (req, res, next) {
+  res.render("index"/*, items*/);
 });
 
 module.exports = router;
