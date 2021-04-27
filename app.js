ab752552fdf3a07cbcfcdb08460289e93b8393a2
benczeg <benczegabor24@gmail.com>
2021-04-27 17:57:06 +0200

    added /try as a fetch example

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var tryRouter = require('./routes/try');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/try', tryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
